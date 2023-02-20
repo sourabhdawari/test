2f317432225127fbda134fd82f24a48cf877aa02
sourabhdawari <sourabhdavari@gmail.com>
2023-02-20 17:08:47 +0530

    final

--- src/Pages/Task1.js
+++ src/Pages/Task1.js
@@ -6,36 +6,43 @@ export default function Task1() {
     const [num2, setNum2] = useState('');
     const [result, setResult] = useState('');
     
-  //operations
-    const Add = () => {
-        setResult(parseFloat(num1) + parseFloat(num2));
-      };
+    function operation(operator) {
+  
+
+      switch (operator) {
+        case "Add":
+          setResult(parseFloat(num1) + parseFloat(num2));
+          break
+        case "Subtract":
+          setResult(parseFloat(num1) - parseFloat(num2));
+          break
+        case "Multiply":
+          setResult(parseFloat(num1) * parseFloat(num2));
+          break
+          case "Divide":
+            setResult(parseFloat(num1) / parseFloat(num2));
+            break
+        default:
+          break;
+      }
+    }
     
-      const Subtract = () => {
-        setResult(parseFloat(num1) - parseFloat(num2));
-      };
-    
-      const Multiply = () => {
-        setResult(parseFloat(num1) * parseFloat(num2));
-      };
-    
-      const Divide = () => {
-        setResult(parseFloat(num1) / parseFloat(num2));
-      };
+
   return (
     <div className='App-header'>
         <div>
         <label>Number 1:</label>
-        <input type="text" class="form-control" id="num1" value={num1} onChange={e => setNum1(e.target.value)} />
+        <input type="number" class="form-control" id="num1" value={num1} onChange={e => setNum1(e.target.value)} />
       </div>
       <div>
         <label>Number 2:</label>
-        <input type="text" class="form-control" id="num2" value={num2} onChange={e => setNum2(e.target.value)} />
+        <input type="number" class="form-control" id="num2" value={num2} onChange={e => setNum2(e.target.value)} />
       </div>
-      <button class="btn btn-primary m-4" onClick={Add}>Add</button>
-      <button class="btn btn-primary m-4" onClick={Subtract}>Subtract</button>
-      <button class="btn btn-primary m-4" onClick={Multiply}>Multiply</button>
-      <button class="btn btn-primary m-4" onClick={Divide}>Divide</button>
+      <button class="btn btn-primary m-4" onClick={()=>operation("Add")}>Add</button>
+      <button class="btn btn-primary m-4" onClick={()=>operation('Subtract')}>Subtract</button>
+      <button class="btn btn-primary m-4" onClick={()=>operation('Multiply')}>Multiply</button>
+      
+      <button class="btn btn-primary m-4" required disabled={num2==="0"}  onClick={()=>operation("Divide")}>Divide</button>
       <div>Result: {result}</div>
       <Link className="btn btn-primary m-5" to="/">Home</Link>
     </div>
